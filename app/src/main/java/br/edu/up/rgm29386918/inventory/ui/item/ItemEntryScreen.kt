f36240762e5c49dccdd512089530f4b58da7aed1
LucasWosch <gabriellucas570@gmail.com>
2024-11-25 15:44:31 -0300

    (feat): adicionando a funcionalidade de salvar o item

--- app/src/main/java/br/edu/up/rgm29386918/inventory/ui/item/ItemEntryScreen.kt
+++ app/src/main/java/br/edu/up/rgm29386918/inventory/ui/item/ItemEntryScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
@@ -45,6 +46,7 @@ import br.edu.up.rgm29386918.inventory.R
 import br.edu.up.rgm29386918.inventory.ui.AppViewModelProvider
 import br.edu.up.rgm29386918.inventory.ui.navigation.NavigationDestination
 import br.edu.up.rgm29386918.inventory.ui.theme.InventoryTheme
+import kotlinx.coroutines.launch
 import java.util.Currency
 import java.util.Locale
 
@@ -61,6 +63,9 @@ fun ItemEntryScreen(
     canNavigateBack: Boolean = true,
     viewModel: ItemEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             InventoryTopAppBar(
@@ -73,7 +78,12 @@ fun ItemEntryScreen(
         ItemEntryBody(
             itemUiState = viewModel.itemUiState,
             onItemValueChange = viewModel::updateUiState,
-            onSaveClick = { },
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveItem()
+                    navigateBack()
+                }
+            },
             modifier = Modifier
                 .padding(
                     start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
