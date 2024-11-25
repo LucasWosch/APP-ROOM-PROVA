479b43e280d7423917d96be2dce6c661f5b443bd
LucasWosch <gabriellucas570@gmail.com>
2024-11-25 15:40:07 -0300

    (feat): passando o repositório para a tela de cadastro do item

--- app/src/main/java/br/edu/up/rgm29386918/inventory/ui/item/ItemEntryViewModel.kt
+++ app/src/main/java/br/edu/up/rgm29386918/inventory/ui/item/ItemEntryViewModel.kt
@@ -21,12 +21,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import br.edu.up.rgm29386918.inventory.data.Item
+import br.edu.up.rgm29386918.inventory.data.ItemsRepository
 import java.text.NumberFormat
 
 /**
  * ViewModel to validate and insert items in the Room database.
  */
-class ItemEntryViewModel : ViewModel() {
+class ItemEntryViewModel(private val itemsRepository: ItemsRepository) : ViewModel() {
 
     /**
      * Holds current item ui state
